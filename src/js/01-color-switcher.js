f3e46806be898fe6914ab8f64dee0903e59ffdee
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-01-28 16:01:51 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,6 +4,8 @@ const refs={
     stopBtn: document.querySelector('button[data-stop]'),
 }
 
+let timerId=null
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
